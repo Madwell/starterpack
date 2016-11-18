609faf74836dc86d7b78936911ef50718da602b3
Walter Mitchell <wmitchell@madwell.com>
2016-11-18 16:45:10 -0500

    scss variables for breakpoints and responsive value function, gulpfile updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp    = require('gulp'),
-	gutil   = require('gulp-util'),
-	uglify  = require('gulp-uglify'),
-	concat  = require('gulp-concat'),
-	sass    = require('gulp-sass'),
-	debug  = require('gulp-debug');
-	livereload  = require('gulp-livereload');
+    gutil   = require('gulp-util'),
+    uglify  = require('gulp-uglify'),
+    concat  = require('gulp-concat'),
+    sass    = require('gulp-sass'),
+    debug  = require('gulp-debug');
+    livereload  = require('gulp-livereload');
 
 // Handle errors
 function errorHandler (error) {
@@ -44,17 +44,28 @@ gulp.task('sass', function() {
         .pipe(livereload());
 });
 
-// Don't bother uglifying JS if working locally, using watch
 gulp.task('watch', function() {
 
     livereload.listen();
+
+    // Watch all SCSS 
     gulp.watch('./public_html/wp-content/themes/CHANGEME/css/*.scss', ['sass']);
-    gulp.watch('./public_html/wp-content/themes/CHANGEME/js/interaction.js', ['concat']);
+
+    // Watch all JS except compiled files
+    gulp.watch([
+            './public_html/wp-content/themes/CHANGEME/js/*.js',
+            '!./public_html/wp-content/themes/CHANGEME/js/app.min.js',
+            '!./public_html/wp-content/themes/CHANGEME/js/app.js'
+        ], [
+            'uglify-concat',
+            'concat'
+        ]);
 
 });
 
 gulp.task('default', [
     'uglify-concat',
+    'concat',
     'sass',
     'watch',
 ]);
